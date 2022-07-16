d99aca33e0ec845a18c22fec0f56fd32c0f9eb41
Prantho-das <pranthokumardas786@gmail.com>
2022-07-16 14:41:34 +0600

    account working

--- .nuxt/router.js
+++ .nuxt/router.js
@@ -4,51 +4,54 @@ import { normalizeURL, decode } from 'ufo'
 import { interopDefault } from './utils'
 import scrollBehavior from './router.scrollBehavior.js'
 
-const _4275d176 = () => interopDefault(import('../pages/admission/index.vue' /* webpackChunkName: "pages/admission/index" */))
-const _78bf08cb = () => interopDefault(import('../pages/app.vue' /* webpackChunkName: "pages/app" */))
-const _1f1dcec8 = () => interopDefault(import('../pages/dum-ac/index.vue' /* webpackChunkName: "pages/dum-ac/index" */))
-const _68d6c877 = () => interopDefault(import('../pages/employee/index.vue' /* webpackChunkName: "pages/employee/index" */))
-const _3fdc10d4 = () => interopDefault(import('../pages/profile/index.vue' /* webpackChunkName: "pages/profile/index" */))
-const _094c3cb0 = () => interopDefault(import('../pages/student/index.vue' /* webpackChunkName: "pages/student/index" */))
-const _d353031c = () => interopDefault(import('../pages/admission/form-import.vue' /* webpackChunkName: "pages/admission/form-import" */))
-const _97d25958 = () => interopDefault(import('../pages/admission/form-sale.vue' /* webpackChunkName: "pages/admission/form-sale" */))
-const _22fdd159 = () => interopDefault(import('../pages/admission/form-stock.vue' /* webpackChunkName: "pages/admission/form-stock" */))
-const _2f8ce56a = () => interopDefault(import('../pages/dum-ac/blog/index.vue' /* webpackChunkName: "pages/dum-ac/blog/index" */))
-const _af4473ec = () => interopDefault(import('../pages/dum-ac/committee/index.vue' /* webpackChunkName: "pages/dum-ac/committee/index" */))
-const _ec440f2c = () => interopDefault(import('../pages/dum-ac/contact.vue' /* webpackChunkName: "pages/dum-ac/contact" */))
-const _f5f12c32 = () => interopDefault(import('../pages/dum-ac/event/index.vue' /* webpackChunkName: "pages/dum-ac/event/index" */))
-const _1c49947f = () => interopDefault(import('../pages/dum-ac/facilitie/index.vue' /* webpackChunkName: "pages/dum-ac/facilitie/index" */))
-const _3499841f = () => interopDefault(import('../pages/dum-ac/gallery/index.vue' /* webpackChunkName: "pages/dum-ac/gallery/index" */))
-const _e54c44fe = () => interopDefault(import('../pages/dum-ac/notice/index.vue' /* webpackChunkName: "pages/dum-ac/notice/index" */))
-const _48a254d1 = () => interopDefault(import('../pages/dum-ac/program/index.vue' /* webpackChunkName: "pages/dum-ac/program/index" */))
-const _298a884a = () => interopDefault(import('../pages/dum-ac/slider/index.vue' /* webpackChunkName: "pages/dum-ac/slider/index" */))
-const _eb70d104 = () => interopDefault(import('../pages/dum-ac/tution/index.vue' /* webpackChunkName: "pages/dum-ac/tution/index" */))
-const _3af8abc0 = () => interopDefault(import('../pages/employee/department/index.vue' /* webpackChunkName: "pages/employee/department/index" */))
-const _4b5b78df = () => interopDefault(import('../pages/employee/designation/index.vue' /* webpackChunkName: "pages/employee/designation/index" */))
-const _2221a4dc = () => interopDefault(import('../pages/employee/employee/index.vue' /* webpackChunkName: "pages/employee/employee/index" */))
-const _e1bd1134 = () => interopDefault(import('../pages/employee/office-time/index.vue' /* webpackChunkName: "pages/employee/office-time/index" */))
-const _33ac7e76 = () => interopDefault(import('../pages/profile/qualification/index.vue' /* webpackChunkName: "pages/profile/qualification/index" */))
-const _28f4c8c8 = () => interopDefault(import('../pages/profile/social/index.vue' /* webpackChunkName: "pages/profile/social/index" */))
-const _25a963c9 = () => interopDefault(import('../pages/profile/training/index.vue' /* webpackChunkName: "pages/profile/training/index" */))
-const _46ac66d3 = () => interopDefault(import('../pages/dum-ac/blog/create.vue' /* webpackChunkName: "pages/dum-ac/blog/create" */))
-const _e9e17418 = () => interopDefault(import('../pages/dum-ac/committee/create.vue' /* webpackChunkName: "pages/dum-ac/committee/create" */))
-const _439a1db7 = () => interopDefault(import('../pages/dum-ac/event/create.vue' /* webpackChunkName: "pages/dum-ac/event/create" */))
-const _d6c573c2 = () => interopDefault(import('../pages/dum-ac/facilitie/create.vue' /* webpackChunkName: "pages/dum-ac/facilitie/create" */))
-const _45961d5d = () => interopDefault(import('../pages/dum-ac/notice/create.vue' /* webpackChunkName: "pages/dum-ac/notice/create" */))
-const _1946dfe6 = () => interopDefault(import('../pages/dum-ac/program/create.vue' /* webpackChunkName: "pages/dum-ac/program/create" */))
-const _2f7acbb4 = () => interopDefault(import('../pages/dum-ac/slider/create.vue' /* webpackChunkName: "pages/dum-ac/slider/create" */))
-const _3340ba00 = () => interopDefault(import('../pages/dum-ac/tution/create.vue' /* webpackChunkName: "pages/dum-ac/tution/create" */))
-const _4bd116fe = () => interopDefault(import('../pages/employee/department/create.vue' /* webpackChunkName: "pages/employee/department/create" */))
-const _47c7edbf = () => interopDefault(import('../pages/employee/designation/create.vue' /* webpackChunkName: "pages/employee/designation/create" */))
-const _49c74162 = () => interopDefault(import('../pages/employee/employee/create.vue' /* webpackChunkName: "pages/employee/employee/create" */))
-const _69979b08 = () => interopDefault(import('../pages/profile/qualification/create.vue' /* webpackChunkName: "pages/profile/qualification/create" */))
-const _a63bbabc = () => interopDefault(import('../pages/profile/social/create.vue' /* webpackChunkName: "pages/profile/social/create" */))
-const _91913fd6 = () => interopDefault(import('../pages/profile/training/create.vue' /* webpackChunkName: "pages/profile/training/create" */))
-const _56181728 = () => interopDefault(import('../pages/dum-ac/committee/update/_id.vue' /* webpackChunkName: "pages/dum-ac/committee/update/_id" */))
-const _01ba6062 = () => interopDefault(import('../pages/employee/employee/detail/_id.vue' /* webpackChunkName: "pages/employee/employee/detail/_id" */))
-const _374488da = () => interopDefault(import('../pages/employee/employee/update/_id.vue' /* webpackChunkName: "pages/employee/employee/update/_id" */))
-const _21940a12 = () => interopDefault(import('../pages/admission/print-receive/_id.vue' /* webpackChunkName: "pages/admission/print-receive/_id" */))
-const _0ae5cb08 = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
+const _fe70eda6 = () => interopDefault(import('../pages/account/index.vue' /* webpackChunkName: "pages/account/index" */))
+const _03b8fec9 = () => interopDefault(import('../pages/admission/index.vue' /* webpackChunkName: "pages/admission/index" */))
+const _4ff55a9e = () => interopDefault(import('../pages/app.vue' /* webpackChunkName: "pages/app" */))
+const _6b4e2429 = () => interopDefault(import('../pages/dum-ac/index.vue' /* webpackChunkName: "pages/dum-ac/index" */))
+const _bec1b678 = () => interopDefault(import('../pages/employee/index.vue' /* webpackChunkName: "pages/employee/index" */))
+const _7e5344ae = () => interopDefault(import('../pages/profile/index.vue' /* webpackChunkName: "pages/profile/index" */))
+const _47c3708a = () => interopDefault(import('../pages/student/index.vue' /* webpackChunkName: "pages/student/index" */))
+const _9f55725a = () => interopDefault(import('../pages/account/class.vue' /* webpackChunkName: "pages/account/class" */))
+const _584eb2b9 = () => interopDefault(import('../pages/account/purpose.vue' /* webpackChunkName: "pages/account/purpose" */))
+const _8f2dbcf6 = () => interopDefault(import('../pages/admission/form-import.vue' /* webpackChunkName: "pages/admission/form-import" */))
+const _cae5c1b2 = () => interopDefault(import('../pages/admission/form-sale.vue' /* webpackChunkName: "pages/admission/form-sale" */))
+const _0b50ffe6 = () => interopDefault(import('../pages/admission/form-stock.vue' /* webpackChunkName: "pages/admission/form-stock" */))
+const _6566b1de = () => interopDefault(import('../pages/dum-ac/blog/index.vue' /* webpackChunkName: "pages/dum-ac/blog/index" */))
+const _6ec0f552 = () => interopDefault(import('../pages/dum-ac/committee/index.vue' /* webpackChunkName: "pages/dum-ac/committee/index" */))
+const _7cb35692 = () => interopDefault(import('../pages/dum-ac/contact.vue' /* webpackChunkName: "pages/dum-ac/contact" */))
+const _a5025098 = () => interopDefault(import('../pages/dum-ac/event/index.vue' /* webpackChunkName: "pages/dum-ac/event/index" */))
+const _3c8b53cc = () => interopDefault(import('../pages/dum-ac/facilitie/index.vue' /* webpackChunkName: "pages/dum-ac/facilitie/index" */))
+const _1cecb2ac = () => interopDefault(import('../pages/dum-ac/gallery/index.vue' /* webpackChunkName: "pages/dum-ac/gallery/index" */))
+const _73d02954 = () => interopDefault(import('../pages/dum-ac/notice/index.vue' /* webpackChunkName: "pages/dum-ac/notice/index" */))
+const _30f5835e = () => interopDefault(import('../pages/dum-ac/program/index.vue' /* webpackChunkName: "pages/dum-ac/program/index" */))
+const _1000d41d = () => interopDefault(import('../pages/dum-ac/slider/index.vue' /* webpackChunkName: "pages/dum-ac/slider/index" */))
+const _70bde351 = () => interopDefault(import('../pages/dum-ac/tution/index.vue' /* webpackChunkName: "pages/dum-ac/tution/index" */))
+const _03e4e55a = () => interopDefault(import('../pages/employee/department/index.vue' /* webpackChunkName: "pages/employee/department/index" */))
+const _2a3a6ca8 = () => interopDefault(import('../pages/employee/designation/index.vue' /* webpackChunkName: "pages/employee/designation/index" */))
+const _0a17cf2f = () => interopDefault(import('../pages/employee/employee/index.vue' /* webpackChunkName: "pages/employee/employee/index" */))
+const _2ea8c833 = () => interopDefault(import('../pages/employee/office-time/index.vue' /* webpackChunkName: "pages/employee/office-time/index" */))
+const _f5e1716e = () => interopDefault(import('../pages/profile/qualification/index.vue' /* webpackChunkName: "pages/profile/qualification/index" */))
+const _584e6bae = () => interopDefault(import('../pages/profile/social/index.vue' /* webpackChunkName: "pages/profile/social/index" */))
+const _45eb2316 = () => interopDefault(import('../pages/profile/training/index.vue' /* webpackChunkName: "pages/profile/training/index" */))
+const _6f23d4a0 = () => interopDefault(import('../pages/dum-ac/blog/create.vue' /* webpackChunkName: "pages/dum-ac/blog/create" */))
+const _73057047 = () => interopDefault(import('../pages/dum-ac/committee/create.vue' /* webpackChunkName: "pages/dum-ac/committee/create" */))
+const _2a10698a = () => interopDefault(import('../pages/dum-ac/event/create.vue' /* webpackChunkName: "pages/dum-ac/event/create" */))
+const _7c937072 = () => interopDefault(import('../pages/dum-ac/facilitie/create.vue' /* webpackChunkName: "pages/dum-ac/facilitie/create" */))
+const _2de94bea = () => interopDefault(import('../pages/dum-ac/notice/create.vue' /* webpackChunkName: "pages/dum-ac/notice/create" */))
+const _156f3320 = () => interopDefault(import('../pages/dum-ac/program/create.vue' /* webpackChunkName: "pages/dum-ac/program/create" */))
+const _17cdfa41 = () => interopDefault(import('../pages/dum-ac/slider/create.vue' /* webpackChunkName: "pages/dum-ac/slider/create" */))
+const _629a5ce6 = () => interopDefault(import('../pages/dum-ac/tution/create.vue' /* webpackChunkName: "pages/dum-ac/tution/create" */))
+const _294f306a = () => interopDefault(import('../pages/employee/department/create.vue' /* webpackChunkName: "pages/employee/department/create" */))
+const _cdaa92dc = () => interopDefault(import('../pages/employee/designation/create.vue' /* webpackChunkName: "pages/employee/designation/create" */))
+const _6096616f = () => interopDefault(import('../pages/employee/employee/create.vue' /* webpackChunkName: "pages/employee/employee/create" */))
+const _448dec95 = () => interopDefault(import('../pages/profile/qualification/create.vue' /* webpackChunkName: "pages/profile/qualification/create" */))
+const _62167496 = () => interopDefault(import('../pages/profile/social/create.vue' /* webpackChunkName: "pages/profile/social/create" */))
+const _c1a4eb30 = () => interopDefault(import('../pages/profile/training/create.vue' /* webpackChunkName: "pages/profile/training/create" */))
+const _2656bd3f = () => interopDefault(import('../pages/dum-ac/committee/update/_id.vue' /* webpackChunkName: "pages/dum-ac/committee/update/_id" */))
+const _469e9c22 = () => interopDefault(import('../pages/employee/employee/detail/_id.vue' /* webpackChunkName: "pages/employee/employee/detail/_id" */))
+const _123ada67 = () => interopDefault(import('../pages/employee/employee/update/_id.vue' /* webpackChunkName: "pages/employee/employee/update/_id" */))
+const _72f6d2e5 = () => interopDefault(import('../pages/admission/print-receive/_id.vue' /* webpackChunkName: "pages/admission/print-receive/_id" */))
+const _451378e2 = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
 
 const emptyFn = () => {}
 
@@ -62,184 +65,196 @@ export const routerOptions = {
   scrollBehavior,
 
   routes: [{
+    path: "/account",
+    component: _fe70eda6,
+    name: "account"
+  }, {
     path: "/admission",
-    component: _4275d176,
+    component: _03b8fec9,
     name: "admission"
   }, {
     path: "/app",
-    component: _78bf08cb,
+    component: _4ff55a9e,
     name: "app"
   }, {
     path: "/dum-ac",
-    component: _1f1dcec8,
+    component: _6b4e2429,
     name: "dum-ac"
   }, {
     path: "/employee",
-    component: _68d6c877,
+    component: _bec1b678,
     name: "employee"
   }, {
     path: "/profile",
-    component: _3fdc10d4,
+    component: _7e5344ae,
     name: "profile"
   }, {
     path: "/student",
-    component: _094c3cb0,
+    component: _47c3708a,
     name: "student"
   }, {
+    path: "/account/class",
+    component: _9f55725a,
+    name: "account-class"
+  }, {
+    path: "/account/purpose",
+    component: _584eb2b9,
+    name: "account-purpose"
+  }, {
     path: "/admission/form-import",
-    component: _d353031c,
+    component: _8f2dbcf6,
     name: "admission-form-import"
   }, {
     path: "/admission/form-sale",
-    component: _97d25958,
+    component: _cae5c1b2,
     name: "admission-form-sale"
   }, {
     path: "/admission/form-stock",
-    component: _22fdd159,
+    component: _0b50ffe6,
     name: "admission-form-stock"
   }, {
     path: "/dum-ac/blog",
-    component: _2f8ce56a,
+    component: _6566b1de,
     name: "dum-ac-blog"
   }, {
     path: "/dum-ac/committee",
-    component: _af4473ec,
+    component: _6ec0f552,
     name: "dum-ac-committee"
   }, {
     path: "/dum-ac/contact",
-    component: _ec440f2c,
+    component: _7cb35692,
     name: "dum-ac-contact"
   }, {
     path: "/dum-ac/event",
-    component: _f5f12c32,
+    component: _a5025098,
     name: "dum-ac-event"
   }, {
     path: "/dum-ac/facilitie",
-    component: _1c49947f,
+    component: _3c8b53cc,
     name: "dum-ac-facilitie"
   }, {
     path: "/dum-ac/gallery",
-    component: _3499841f,
+    component: _1cecb2ac,
     name: "dum-ac-gallery"
   }, {
     path: "/dum-ac/notice",
-    component: _e54c44fe,
+    component: _73d02954,
     name: "dum-ac-notice"
   }, {
     path: "/dum-ac/program",
-    component: _48a254d1,
+    component: _30f5835e,
     name: "dum-ac-program"
   }, {
     path: "/dum-ac/slider",
-    component: _298a884a,
+    component: _1000d41d,
     name: "dum-ac-slider"
   }, {
     path: "/dum-ac/tution",
-    component: _eb70d104,
+    component: _70bde351,
     name: "dum-ac-tution"
   }, {
     path: "/employee/department",
-    component: _3af8abc0,
+    component: _03e4e55a,
     name: "employee-department"
   }, {
     path: "/employee/designation",
-    component: _4b5b78df,
+    component: _2a3a6ca8,
     name: "employee-designation"
   }, {
     path: "/employee/employee",
-    component: _2221a4dc,
+    component: _0a17cf2f,
     name: "employee-employee"
   }, {
     path: "/employee/office-time",
-    component: _e1bd1134,
+    component: _2ea8c833,
     name: "employee-office-time"
   }, {
     path: "/profile/qualification",
-    component: _33ac7e76,
+    component: _f5e1716e,
     name: "profile-qualification"
   }, {
     path: "/profile/social",
-    component: _28f4c8c8,
+    component: _584e6bae,
     name: "profile-social"
   }, {
     path: "/profile/training",
-    component: _25a963c9,
+    component: _45eb2316,
     name: "profile-training"
   }, {
     path: "/dum-ac/blog/create",
-    component: _46ac66d3,
+    component: _6f23d4a0,
     name: "dum-ac-blog-create"
   }, {
     path: "/dum-ac/committee/create",
-    component: _e9e17418,
+    component: _73057047,
     name: "dum-ac-committee-create"
   }, {
     path: "/dum-ac/event/create",
-    component: _439a1db7,
+    component: _2a10698a,
     name: "dum-ac-event-create"
   }, {
     path: "/dum-ac/facilitie/create",
-    component: _d6c573c2,
+    component: _7c937072,
     name: "dum-ac-facilitie-create"
   }, {
     path: "/dum-ac/notice/create",
-    component: _45961d5d,
+    component: _2de94bea,
     name: "dum-ac-notice-create"
   }, {
     path: "/dum-ac/program/create",
-    component: _1946dfe6,
+    component: _156f3320,
     name: "dum-ac-program-create"
   }, {
     path: "/dum-ac/slider/create",
-    component: _2f7acbb4,
+    component: _17cdfa41,
     name: "dum-ac-slider-create"
   }, {
     path: "/dum-ac/tution/create",
-    component: _3340ba00,
+    component: _629a5ce6,
     name: "dum-ac-tution-create"
   }, {
     path: "/employee/department/create",
-    component: _4bd116fe,
+    component: _294f306a,
     name: "employee-department-create"
   }, {
     path: "/employee/designation/create",
-    component: _47c7edbf,
+    component: _cdaa92dc,
     name: "employee-designation-create"
   }, {
     path: "/employee/employee/create",
-    component: _49c74162,
+    component: _6096616f,
     name: "employee-employee-create"
   }, {
     path: "/profile/qualification/create",
-    component: _69979b08,
+    component: _448dec95,
     name: "profile-qualification-create"
   }, {
     path: "/profile/social/create",
-    component: _a63bbabc,
+    component: _62167496,
     name: "profile-social-create"
   }, {
     path: "/profile/training/create",
-    component: _91913fd6,
+    component: _c1a4eb30,
     name: "profile-training-create"
   }, {
     path: "/dum-ac/committee/update/:id",
-    component: _56181728,
+    component: _2656bd3f,
     name: "dum-ac-committee-update-id"
   }, {
     path: "/employee/employee/detail/:id?",
-    component: _01ba6062,
+    component: _469e9c22,
     name: "employee-employee-detail-id"
   }, {
     path: "/employee/employee/update/:id?",
-    component: _374488da,
+    component: _123ada67,
     name: "employee-employee-update-id"
   }, {
     path: "/admission/print-receive/:id?",
-    component: _21940a12,
+    component: _72f6d2e5,
     name: "admission-print-receive-id"
   }, {
     path: "/",
-    component: _0ae5cb08,
+    component: _451378e2,
     name: "index"
   }],
 
--- nuxt.config.js
+++ nuxt.config.js
@@ -12,46 +12,46 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      // {rel: 'icon', type: 'image/x-icon', href: '/favicon.png'},      
-      {rel: 'stylesheet', id: 'bootstrap-style', href: '/css/bootstrap.min.css'},      
+      // {rel: 'icon', type: 'image/x-icon', href: '/favicon.png'},
+      {rel: 'stylesheet', id: 'bootstrap-style', href: '/css/bootstrap.min.css'},
       {rel: 'stylesheet', id: 'font-awesome-style', href: '/css/font-awesome.min.css'},
       // {rel: 'stylesheet', id: 'style-style', href: '/css/style.css'},
 
 
-      
+
     ],
 
     script: [
-      
-      {src: '/js/jquery.min.js', defer: true},  
+
+      {src: '/js/jquery.min.js', defer: true},
       {src: '/js/main.js', defer: true},
       {src: '/js/bootstrap.min.js', defer: true},
       {src: '/js/bootstrap.bundle.min.js', defer: true},
-      
 
-      
+
+
     ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [   
+  css: [
    '@/assets/css/main.css',
   //  '/css/font-awesome.min.css',
-  
-  
+
+
   ],
 
 
-  
+
   plugins: [
     {src: 'plugins/owl.js', ssr: false},
     {src: '~/plugins/vue-toaster.js', ssr: false},
   ],
 
-  
+
   components: true,
 
-  
+
   buildModules: [
     '@nuxtjs/google-fonts'
   ],
@@ -65,18 +65,18 @@ export default {
         ital: [100]
       }
     }
-   
+
   },
 
   router: {
     middleware: 'auth'
   },
-  
+
   auth: {
     strategies: {
       laravelSanctum: {
         provider: 'laravel/sanctum',
-        url: 'http://localhost:8000',
+        url: 'http://localhost:8001',
         token: {
           property: 'token',
           global: true,
@@ -86,14 +86,14 @@ export default {
         user: {
           property: false,
           autoFetch: true
-        },  
+        },
         endpoints: {
           login: { url: '/api/login', method: 'post'},
           logout: { url: '/api/logout', method: 'post' },
           user: { url: '/api/user', method: 'get'}
-        },        
+        },
       },
-     
+
     },
      redirect:{
           login: '/',
@@ -101,14 +101,14 @@ export default {
           home:'/app',
         }
   },
-      
 
- 
-  axios: {   
-    baseURL: 'http://localhost:8000/api',
+
+
+  axios: {
+    baseURL: 'http://localhost:8001/api',
     credentials: true
   },
-  
+
 
 
   modules: [
@@ -117,12 +117,12 @@ export default {
     'bootstrap-vue/nuxt',
     'nuxt-sweetalert2',
   ],
- 
- 
- 
+
+
+
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
   }
-  
+
 }
